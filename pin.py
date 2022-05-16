269ead190fe6d1de6155202c6067abf1322ab4b4
Kerbx <r3ostat@ya.ru>
2022-05-16 21:43:45 +0300

    Created class for GPIO manage.

---
+++ pin.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+
+
+class Pin():
+    def __init__(self):
+        ######################
+        # CONSTANTS FOR GPIO #
+        ######################
+        self.pins = [5, 6, 13, 16, 19, 20, 21, 26]
+        
+        #################
+        # GPIO SETTINGS #
+        #################
+
+        GPIO.setmode(GPIO.BCM)
+        for pin in self.pins:
+            GPIO.setup(pin, GPIO.OUTPUT)
+            
+                    
+    def offAll(self) -> None:
+        """Off all reley.
+        """
+        for pin in self.pins:
+            GPIO.output(pin, GPIO.HIGH)
+            
+    
+    def onAll(self) -> None:
+        """On all reley.
+        """
+        for pin in self.pins:
+            GPIO.output(pin, GPIO.LOW)
+            
+    
+    def offPin(self, pin: int) -> None:
+        """Off the reley.
+
+        Args:
+            pin (int): Possible values: [5, 6, 13, 16, 19, 20, 21, 26].
+        """
+        GPIO.output(pin, GPIO.HIGH)
+        
+    
+    def onPin(self, pin: int) -> None:
+        """On the reley.
+
+        Args:
+            pin (int): Possible values: [5, 6, 13, 16, 19, 20, 21, 26].
+        """
+        GPIO.output(pin, GPIO.LOW)
